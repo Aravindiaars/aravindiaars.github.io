c73daf36cab6b3b7ba0ecf35f25159cf9c5f5f8e
Aravindiaars <44970505+Aravindiaars@users.noreply.github.com>
2023-05-03 11:10:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ clipboard.on('error', function(e) {
   console.log(e);
 });
 
-async function copyImageToClipBoardOtherBrowsers() {
-	alert("NEW");
-  let input = document.getElementById("copyme");
-  if(!input.files?.length){
-    alert('Select files before copy')
+
+async function copyImage1() {
+  let input = document.getElementById("file");
+  if (!input.files?.length) {
+    alert("Select files before copy");
   }
   const blob = new Blob(["sample 2"], { type: "text/plain" });
   let clipboardItem = new ClipboardItem({
@@ -26,8 +26,12 @@ async function copyImageToClipBoardOtherBrowsers() {
   let response = await navigator.clipboard.write([clipboardItem]);
   console.log(response);
 }
+async function paste() {
+ const data = await navigator.clipboard.read();
+  console.log(data);
+}
 
-async function copyImage1(data) {
+async function copyImage(data) {
 	alert(data);
   let response = await  navigator.clipboard
             .write(data)
@@ -46,7 +50,7 @@ async function copyImage1(data) {
 const isNotFirefox = navigator.userAgent.indexOf("Firefox") < 0;
 
 
-const copyImageToClipBoardOtherBrowsers1 = () => {
+const copyImageToClipBoardOtherBrowsers = () => {
 	try {
 	alert(isNotFirefox);
   if(isNotFirefox) {
